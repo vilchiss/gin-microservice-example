4ee5ddd96535eda91937ab93062666899e05a9e1
Luis Oropeza <luis.oropeza.129@gmail.com>
2022-04-17 23:47:49 -0500

    ADD: path variable

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import "github.com/gin-gonic/gin"
 
 func IndexHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
 	c.JSON(200, gin.H{
-		"message": "hello world",
+		"message": "hello " + name,
 	})
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/", IndexHandler)
+	router.GET("/:name", IndexHandler)
 	router.Run()
 }
