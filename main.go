32f7d8ade627466f582b3e95d04e7e999d954c1d
Luis Oropeza <luis.oropeza.129@gmail.com>
2022-04-18 00:01:00 -0500

    UPDATE: boilerplate

--- main.go
+++ main.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-	"encoding/xml"
-
 	"github.com/gin-gonic/gin"
 )
 
-type Person struct {
-	XMLName   xml.Name `xml:"person"`
-	FirstName string   `xml:"firstName,attr"`
-	LastName  string   `xml:"lastName,attr"`
-}
-
-func IndexHandler(c *gin.Context) {
-	c.XML(200, Person{
-		FirstName: "Marco",
-		LastName:  "Aurelio",
-	})
-}
-
 func main() {
 	router := gin.Default()
-	router.GET("/", IndexHandler)
 	router.Run()
 }
