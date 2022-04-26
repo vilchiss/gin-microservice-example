d63b5f0a227e9315a0cc9a98e2938d3baa2fd172
Luis Oropeza <luis.oropeza.129@gmail.com>
2022-04-25 23:27:16 -0500

    REFACTOR: create a recipe using mongodb

--- main.go
+++ main.go
@@ -84,7 +84,16 @@ func NewRecipeHandler(c *gin.Context) {
 
 	recipe.ID = primitive.NewObjectID()
 	recipe.PublishedAt = time.Now()
-	recipes = append(recipes, recipe)
+	_, err = collection.InsertOne(ctx, recipe)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Error while inserting a new recipe",
+		})
+
+		return
+	}
+
 	c.JSON(http.StatusOK, recipe)
 }
 
